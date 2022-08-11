4207dd84769f2ff9ee783c1174e4ad47b30791b4
solchan98 <dev.sol0427@gmail.com>
2022-08-11 16:55:16 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/example/random_question_study/RandomQuestionStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.random_question_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomQuestionStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomQuestionStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/random_question_study/RandomQuestionStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.random_question_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomQuestionStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
