0211d2fce08cd355a0de91a66193447ed838ed39
solchan98 <dev.sol0427@gmail.com>
2022-08-12 15:51:02 +0900

    feat: 구현 계획 기능 인터페이스를 통한 임시 명세

---
+++ src/main/java/com/example/rqs/core/space/service/SpaceService.java
@@ -0,0 +1,17 @@
+package com.example.rqs.core.space.service;
+
+public interface SpaceService {
+    void createSpace();
+
+    void updateTitle();
+
+    void changeVisibility();
+
+    void addNewMember();
+
+    void changeMemberRole();
+
+    void deleteMember();
+
+    void deleteSpace();
+}
