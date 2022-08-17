b2f4d8010f458ee0a677d3b0c07607db40560cb3
solchan98 <dev.sol0427@gmail.com>
2022-08-17 23:59:39 +0900

    feat: 유저 정보 조회 API 구현

--- src/main/java/com/example/rqs/api/member/MemberController.java
+++ src/main/java/com/example/rqs/api/member/MemberController.java
@@ -1,9 +1,11 @@
 package com.example.rqs.api.member;
 
 import com.example.rqs.api.jwt.JwtProvider;
+import com.example.rqs.api.jwt.MemberDetails;
 import com.example.rqs.core.member.service.MemberService;
 import com.example.rqs.core.member.service.dtos.*;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
@@ -39,5 +41,11 @@ public class MemberController {
         String atk = jwtProvider.createAccessToken(memberDto.getEmail(), memberDto.getNickname(), "USER");
         LoginResponse loginResponse = LoginResponse.of(memberDto, atk);
         return ResponseEntity.ok(loginResponse);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<MemberDto> getMemberInfo(@AuthenticationPrincipal MemberDetails memberDetails) {
+        MemberDto memberDto = MemberDto.of(memberDetails.getMember());
+        return ResponseEntity.ok(memberDto);
     }
 }
