2cdf59a3168b0380b575a466f78b41660a03151b
solchan98 <dev.sol0427@gmail.com>
2022-08-23 20:14:04 +0900

    feat: 아이템 업데이트 API 테스트 케이스 작성

--- src/test/java/com/example/rqs/api/item/ItemControllerTest.java
+++ src/test/java/com/example/rqs/api/item/ItemControllerTest.java
@@ -16,11 +16,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest({ItemController.class, CreateItemValidator.class, JwtProvider.class, RedisDao.class})
+@WebMvcTest({ItemController.class, CreateItemValidator.class, UpdateItemValidator.class, JwtProvider.class, RedisDao.class})
 @DisplayName("아이템 컨트롤러 테스트")
 public class ItemControllerTest {
 
@@ -64,6 +64,43 @@ public class ItemControllerTest {
 
         ResultActions perform = mockMvc.perform(
                 post("/api/v1/item")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(req)
+                        .with(csrf()));
+
+        perform
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("요청 데이터를 확인하세요."));
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("아이템 업데이트, Question필드 비어있는 경우 예외 처리 400")
+    void updateItemFailByNullQuestionField() throws Exception {
+        CreateItemDto createItemDto = new CreateItemDto(1L, "", "답변이란당", "없엉, 틀려랑, 땡!");
+        String req = objectMapper.writeValueAsString(createItemDto);
+
+        ResultActions perform = mockMvc.perform(
+                put("/api/v1/item")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(req)
+                        .with(csrf()));
+
+        perform
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("요청 데이터를 확인하세요."));
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("아이템 업데이트, Answer필드 비어있는 경우 예외 처리 400")
+    void updateItemFailByNullAnswerField() throws Exception {
+        CreateItemDto createItemDto = new CreateItemDto(
+                1L, "프로세스 어쩌구 저쩌구", "", "없엉, 틀려랑, 땡!");
+        String req = objectMapper.writeValueAsString(createItemDto);
+
+        ResultActions perform = mockMvc.perform(
+                put("/api/v1/item")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(req)
                         .with(csrf()));
