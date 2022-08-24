fde794c95a966ce7ff57ea3234d71d3ea3184c1e
solchan98 <dev.sol0427@gmail.com>
2022-08-24 16:17:45 +0900

    refactor: cors DELETE 메서드 추가 허용

--- src/main/java/com/example/rqs/api/config/ApiConfig.java
+++ src/main/java/com/example/rqs/api/config/ApiConfig.java
@@ -54,7 +54,7 @@ public class ApiConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOriginPatterns(List.of("*"));
-        configuration.setAllowedMethods(List.of("HEAD", "GET", "POST", "PUT", "PATCH"));
+        configuration.setAllowedMethods(List.of("HEAD", "GET", "POST", "PUT", "PATCH", "DELETE"));
         configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
