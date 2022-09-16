6b9b0206ea673cc3863cac728741de490f343619
solchan98 <dev.sol0427@gmail.com>
2022-09-16 09:48:57 +0900

    refactor: 스페이스 응답 DTO memberJoinedAt 필드 추가

--- src/main/java/com/example/rqs/core/space/service/dtos/SpaceResponse.java
+++ src/main/java/com/example/rqs/core/space/service/dtos/SpaceResponse.java
@@ -1,16 +1,13 @@
 package com.example.rqs.core.space.service.dtos;
 
-import com.example.rqs.core.space.Space;
-import com.example.rqs.core.space.SpaceMember;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import com.example.rqs.core.space.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
-@ToString
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class SpaceResponse {
 
     private Long spaceId;
@@ -23,30 +20,22 @@ public class SpaceResponse {
 
     private Integer itemCount;
 
+    private LocalDateTime memberJoinedAt;
+
     private String authority;
 
     private LocalDateTime createdAt;
 
     private LocalDateTime updatedAt;
 
-    private SpaceResponse(Long spaceId, String title, boolean visibility, Integer itemCount, Integer spaceMemberCount, String authority, LocalDateTime createdAt, LocalDateTime updatedAt) {
-        this.spaceId = spaceId;
-        this.title = title;
-        this.visibility = visibility;
-        this.itemCount = itemCount;
-        this.spaceMemberCount = spaceMemberCount;
-        this.authority = authority;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
-
     public static SpaceResponse createByGuest(Space space) {
         return new SpaceResponse(
                 space.getSpaceId(),
                 space.getTitle(),
                 space.isVisibility(),
-                space.getItemList().size(),
                 space.getSpaceMemberList().size(),
+                space.getItemList().size(),
+                null,
                 "GUEST",
                 space.getCreatedAt(),
                 space.getUpdatedAt());
@@ -57,8 +46,9 @@ public class SpaceResponse {
                 space.getSpaceId(),
                 space.getTitle(),
                 space.isVisibility(),
-                space.getItemList().size(),
                 space.getSpaceMemberList().size(),
+                space.getItemList().size(),
+                reader.getJoinedAt(),
                 reader.getRole(),
                 space.getCreatedAt(),
                 space.getUpdatedAt());
