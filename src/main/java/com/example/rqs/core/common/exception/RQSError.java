9a922f5258420297e4e3fd9405b6fc236d6468b8
solchan98 <dev.sol0427@gmail.com>
2022-09-18 15:21:06 +0900

    squash! refactor: 스페이스 멤버 추가 서비스, 이미 존재하는 멤버인 경우 예외 처리 추가

--- src/main/java/com/example/rqs/core/common/exception/RQSError.java
+++ src/main/java/com/example/rqs/core/common/exception/RQSError.java
@@ -8,6 +8,7 @@ public class RQSError {
     public static String SPACE_IS_NOT_EXIST = "스페이스가 존재하지 않습니다.";
     public static String SPACE_IS_EMPTY = "스페이스가 비어있습니다.";
     public static String SPACE_MEMBER_NOT_FOUND = "해당 멤버가 스페이스에 존재하지 않습니다.";
+    public static String SPACE_MEMBER_ALREADY_EXIST = "이미 스페이스에 존재하는 멤버입니다.";
 
     public static String ITEM_IS_NOT_EXIST_IN_SPACE = "스페이스에 해당 아이템이 존재하지 않습니다.";
 }
