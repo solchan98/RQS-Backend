f6bfc7b26a461994f0adbb90aa6ef99f71d13f43
solchan98 <dev.sol0427@gmail.com>
2022-11-02 15:18:17 +0900

    ci/cd: 배포 스크립트 도커 네트워크 설정 부분 수정

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 sudo docker rm -f app
 sudo docker pull quizbox/app:latest
-sudo docker run --name app -d -p 8080:8080 --network app quizbox/app:latest
+sudo docker run --name app -d -p 8080:8080 --network quizbox quizbox/app:latest
 sudo docker image prune -f
