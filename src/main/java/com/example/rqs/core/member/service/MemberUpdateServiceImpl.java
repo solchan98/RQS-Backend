11459ff0b6c096168b612f74eef90973df35fcd5
solchan98 <dev.sol0427@gmail.com>
2023-01-28 16:07:34 +0900

    feat: Member Update 코드 수정 MemberDetails를 통해 받은 Member 객체는 준영속성 상태 따라서 직접 save 호출하도록 수정

--- src/main/java/com/example/rqs/core/member/service/MemberUpdateServiceImpl.java
+++ src/main/java/com/example/rqs/core/member/service/MemberUpdateServiceImpl.java
@@ -1,29 +1,37 @@
 package com.example.rqs.core.member.service;
 
 import com.example.rqs.core.member.Member;
+import com.example.rqs.core.member.repository.MemberRepository;
 import com.example.rqs.core.member.service.dtos.MemberDto;
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class MemberUpdateServiceImpl implements MemberUpdateService {
+    private final MemberRepository memberRepository;
 
     @Override
     public MemberDto updateNickname(Member member, String nickname) {
         member.updateNickname(nickname);
+        memberRepository.save(member);
         return MemberDto.of(member);
     }
 
     @Override
     public MemberDto updateDescription(Member member, String description) {
         member.updateDescription(description);
+        memberRepository.save(member);
         return MemberDto.of(member);
     }
 
     @Override
     public MemberDto updateAvatar(Member member, String avatarUrl) {
         member.updateAvatar(avatarUrl);
+        memberRepository.save(member);
         return MemberDto.of(member);
     }
 }
