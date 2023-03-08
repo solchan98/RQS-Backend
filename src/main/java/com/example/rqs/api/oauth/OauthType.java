59703086465b75a9d3128c967528459e95fd2538
solchan98 <dev.sol0427@gmail.com>
2023-03-08 15:14:09 +0900

    feat: OauthType Enum 작성 GOOGLE TYPE 추가 및 valueOfType 메서드 구현

---
+++ src/main/java/com/example/rqs/api/oauth/OauthType.java
@@ -0,0 +1,26 @@
+package com.example.rqs.api.oauth;
+
+import com.example.rqs.core.common.exception.BadRequestException;
+
+import java.util.Arrays;
+
+public enum OauthType {
+    GOOGLE("google");
+
+    final String type;
+
+    OauthType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public static OauthType valueOfType(String type) {
+        return Arrays
+                .stream(OauthType.values()).filter((e) -> e.type.equals(type))
+                .findFirst()
+                .orElseThrow(() -> new BadRequestException("Not Supported Oauth Type"));
+    }
+}
