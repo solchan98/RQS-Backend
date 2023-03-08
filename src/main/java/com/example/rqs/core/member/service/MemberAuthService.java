ab0499abab9527badd3d0bad12c5b47e1a2dc4de
solchan98 <dev.sol0427@gmail.com>
2023-03-08 15:46:03 +0900

    feat: MemberAuthService oauth login 구현 이미 존재하는 oauth 유저인 경우에는 로그인, 처음 로그인하는 oauth 유저인 경우에는 회원가입 진행

--- src/main/java/com/example/rqs/core/member/service/MemberAuthService.java
+++ src/main/java/com/example/rqs/core/member/service/MemberAuthService.java
@@ -3,11 +3,13 @@ package com.example.rqs.core.member.service;
 import com.example.rqs.core.member.Member;
 import com.example.rqs.core.member.service.dtos.LoginDto;
 import com.example.rqs.core.member.service.dtos.MemberDto;
+import com.example.rqs.core.member.service.dtos.OauthLoginDto;
 
 import java.util.Optional;
 
 public interface MemberAuthService {
     MemberDto login(LoginDto loginDto);
+    MemberDto oauthLogin(OauthLoginDto oauthLoginDto);
     boolean logout();
     boolean existEmail(String email);
     Optional<Member> getMember(long memberId);
