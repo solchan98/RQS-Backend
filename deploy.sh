3e6e471a2c78e4518559eeb187cc27a56436c3c7
solchan98 <dev.sol0427@gmail.com>
2023-03-09 17:37:05 +0900

    feat: 배포 스크립트 수정

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 sudo docker rm -f app
+sudo docker rmi quizbox/app
 sudo docker pull quizbox/app:latest
 sudo docker run --name app -d -p 8080:8080 --network quizbox quizbox/app:latest
 sudo docker image prune -f
