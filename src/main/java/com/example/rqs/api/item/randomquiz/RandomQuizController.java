b4de60580167f1130b058cb076cf793649d2398d
solchan98 <dev.sol0427@gmail.com>
2023-03-12 01:43:19 +0900

    feat: RandomQuizController pickRandomQuiz API 구성 pickRandomQuiz API 1차 구성 첫 조회인 경우 start 메서드 우선 호출, 이후 random item id를 뽑아 응답하도록 구성

--- src/main/java/com/example/rqs/api/item/randomquiz/RandomQuizController.java
+++ src/main/java/com/example/rqs/api/item/randomquiz/RandomQuizController.java
@@ -3,16 +3,20 @@ package com.example.rqs.api.item.randomquiz;
 import com.example.rqs.api.cache.quiz.QuizCache;
 import com.example.rqs.api.cache.quiz.QuizCacheService;
 import com.example.rqs.api.jwt.MemberDetails;
+import com.example.rqs.core.item.service.ItemReadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/quiz")
 @RequiredArgsConstructor
 public class RandomQuizController {
     private final QuizCacheService quizCacheService;
+    private final ItemReadService itemReadService;
 
     // was started QuizGame
     @GetMapping("/progress/{spaceId}")
@@ -27,5 +31,26 @@ public class RandomQuizController {
             return ResponseEntity.ok(InProgressResponse.from(quizCache));
         }
         return ResponseEntity.ok(InProgressResponse.of(false));
+    }
+
+
+    // get random quiz
+    @GetMapping("/pick/{spaceId}")
+    // TODO: Object -> RandomItem
+    public ResponseEntity<Object> pickRandomQuiz(
+            @AuthenticationPrincipal MemberDetails memberDetails,
+            @PathVariable("spaceId") Long spaceId
+    ) {
+        Long memberId = memberDetails.getMember().getMemberId();
+        boolean inProgress = quizCacheService.inProgress(spaceId, memberId);
+        if (!inProgress) {
+            /** TODO: itemReadService.getItemIds(spaceId);
+             * Remove itemRandomService
+             */
+            quizCacheService.start(spaceId, memberId, List.of());
+        }
+        Long randomQuizId = quizCacheService.pickRandomQuizId(spaceId, memberId);
+        // TODO: null -> RandomItem
+        return ResponseEntity.ok(null);
     }
 }
