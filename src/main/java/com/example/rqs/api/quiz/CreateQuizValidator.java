becde7c3ea0bd93ebe3d30f4530e2dbc054a7f55
solchan98 <dev.sol0427@gmail.com>
2023-03-13 16:45:18 +0900

    feat: Quiz 생성 시, 타입에 따른 검증 로직 추가

--- src/main/java/com/example/rqs/api/quiz/CreateQuizValidator.java
+++ src/main/java/com/example/rqs/api/quiz/CreateQuizValidator.java
@@ -21,6 +21,24 @@ public class CreateQuizValidator implements Validator {
                 createQuizDto.getQuestion().isEmpty()
                 || createQuizDto.getCreateAnswers().isEmpty();
 
+        if (createQuizDto.getType().equals("form")) {
+            checkIsValidToForm(createQuizDto);
+        } else {
+            checkIsValidMuti(createQuizDto);
+        }
+
         if (isEmpty) throw new BadRequestException();
+    }
+
+    private void checkIsValidToForm(CreateQuizDto createQuizDto) {
+        if (createQuizDto.getCreateAnswers().size() > 1) {
+            throw new BadRequestException("정답을 2개 이상 작성할 수 없습니다.");
+        }
+    }
+
+    private void checkIsValidMuti(CreateQuizDto createQuizDto){
+        if (createQuizDto.getCreateAnswers().size() < 2) {
+            throw new BadRequestException("정답을 최소 2개 이상 작성해야 합니다.");
+        }
     }
 }
