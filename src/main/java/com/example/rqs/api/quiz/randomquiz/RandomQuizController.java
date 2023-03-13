43a900165983aff4591c9d644677167018db7514
solchan98 <dev.sol0427@gmail.com>
2023-03-13 20:56:34 +0900

    feat: 랜덤 퀴즈 api 수정

--- src/main/java/com/example/rqs/api/quiz/randomquiz/RandomQuizController.java
+++ src/main/java/com/example/rqs/api/quiz/randomquiz/RandomQuizController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/quiz")
+@RequestMapping("/api/v1/quizgame")
 @RequiredArgsConstructor
 public class RandomQuizController {
     private final QuizCacheService quizCacheService;
